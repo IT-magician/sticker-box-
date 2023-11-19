a559c22282941807a38538974cf6db9a3de099fa
hoan123432 <hoan123432@naver.com>
2023-11-19 20:08:09 +0900

    add: 스티커 항목 이동 - 기초 요구사항 반영완료. 버그 : 가장 가까운 부모 선택하는 거랑 가장 가까운 형제 선택하는 알고리즘 수정 필요 <- 다른거 하고 시간 남으면 하지.. 미정으로 남겨두기!

--- main.js
+++ main.js
@@ -11,106 +11,6 @@ function debounce(callback, limit = 100) {
   };
 }
 
-// document.querySelectorAll(".draggable").forEach((element) => {
-//   // 마우스의 위치값 저장
-//   let x = 0;
-//   let y = 0;
-
-//   let ghost = null;
-
-//   // 마우스 누른 순간 이벤트
-//   const mouseDownHandler = function (event) {
-//     ghost = element.cloneNode(true);
-//     ghost.classList.add("ghost");
-//     ghost.innerHTML += "ghost";
-//     element.after(ghost);
-
-//     // 누른 마우스 위치값을 가져와 저장
-//     x = event.clientX;
-//     y = event.clientY;
-
-//     // 마우스 이동시 초기 위치와의 거리차 계산
-//     const dx = event.clientX - x;
-//     const dy = event.clientY - y;
-
-//     // 마우스 이동 거리 만큼 Element의 top, left 위치값에 반영
-//     element.style.top = `${element.offsetTop + dy}px`;
-//     element.style.left = `${element.offsetLeft + dx}px`;
-
-//     element.style.position = "absolute";
-
-//     // 마우스 이동 및 해제 이벤트를 등록
-//     document.addEventListener("mousemove", mouseMoveHandler);
-//     document.addEventListener("mouseup", mouseUpHandler);
-//   };
-
-//   const mouseMoveHandler = function (event) {
-//     debounce(() => {}, 1);
-//     // 마우스 이동시 초기 위치와의 거리차 계산
-//     const dx = event.clientX - x;
-//     const dy = event.clientY - y;
-
-//     // 마우스 이동 거리 만큼 Element의 top, left 위치값에 반영
-//     element.style.top = `${element.offsetTop + dy}px`;
-//     element.style.left = `${element.offsetLeft + dx}px`;
-
-//     // 기준 위치 값을 현재 마우스 위치로 update
-//     x = event.clientX;
-//     y = event.clientY;
-//   };
-
-//   const mouseUpHandler = function () {
-//     // 마우스가 해제되면 이벤트도 같이 해제
-//     document.removeEventListener("mousemove", mouseMoveHandler);
-//     document.removeEventListener("mouseup", mouseUpHandler);
-
-//     ghost.remove();
-//     element.removeAttribute("style");
-//   };
-
-//   element.addEventListener("mousedown", mouseDownHandler);
-// });
-
-// document.querySelectorAll(".container").forEach((element) => {
-//   let shiftX;
-//   let shiftY;
-
-//   element.addEventListener("mousedown", (event) => {
-//     // 마우스 누른 순간 이벤트
-//     const mouseDownHandler = function (event) {
-//       shiftX = event.clientX - element.getBoundingClientRect().left;
-//       shiftY = event.clientY - element.getBoundingClientRect().top;
-
-//       element.style.position = "absolute";
-//       element.style.zIndex++;
-
-//       element.style.left = event.pageX - shiftX + "px";
-//       element.style.top = event.pageY - shiftY + "px";
-
-//       // 마우스 이동 및 해제 이벤트를 등록
-//       document.addEventListener("mousemove", mouseMoveHandler);
-//       document.addEventListener("mouseup", mouseUpHandler);
-
-//       console.log("container mousedown");
-//     };
-
-//     const mouseMoveHandler = function (event) {
-//       element.style.left = event.pageX - shiftX + "px";
-//       element.style.top = event.pageY - shiftY + "px";
-
-//       console.log("container mousemove");
-//     };
-
-//     const mouseUpHandler = function () {
-//       // 마우스가 해제되면 이벤트도 같이 해제
-//       document.removeEventListener("mousemove", mouseMoveHandler);
-//       document.removeEventListener("mouseup", mouseUpHandler);
-//     };
-
-//     element.addEventListener("mousedown", mouseDownHandler);
-//   });
-// });
-
 (() => {
   const board = document.querySelector(".board");
 
@@ -129,14 +29,18 @@ function debounce(callback, limit = 100) {
 
       element.style.zIndex =
         findMaxZIndex(board.querySelectorAll(".container")) + 1;
+      element.style.margin = 0;
     } else if (WhoIam(event.target, "draggable")) {
       element = event.target;
-      element.style.zIndex =
-        findMaxZIndex(board.querySelectorAll(".draggable")) + 1;
+
       ghost = element.cloneNode(true);
       ghost.classList.add("ghost");
       ghost.innerHTML += "ghost";
       event.target.after(ghost);
+
+      element.style.zIndex =
+        findMaxZIndex(board.querySelectorAll(".draggable")) + 1;
+      element.style.margin = 0;
     }
 
     // 움직일 대상에 대한 처리
